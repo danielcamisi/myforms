2b12d2a568bc85548c2aa361967cbf0ac97be04a
danielcamisi <danielcamillo2010@hotmail.com>
2024-03-19 21:25:48 -0300

    other commit

--- src/app/component/moment-form/moment-form.component.ts
+++ src/app/component/moment-form/moment-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -8,9 +10,32 @@ import { Component, OnInit, Input } from '@angular/core';
 export class MomentFormComponent implements OnInit{
   @Input() btnText!:string;
 
+  mommentForm!: FormGroup;
+
   constructor() { }
 
-  ngOnInit():void {
+  ngOnInit():void { 
+    this.mommentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('',[Validators.required]),
+      description: new FormControl('',[Validators.required]),
+      Image: new FormControl(''),
+      
+    });
+  }
 
+  get title(){
+    return this.mommentForm.get('title')!;
+  }
+
+  get description(){
+    return this.mommentForm.get('title')!;
+  }
+
+  submit(){
+    if(this.mommentForm.invalid){
+      return;
+    }
+    console.log("Enviou o feedback!")
   }
 }
